b0e881341c42f22b211d8f9c08824f57fe363ec1
Francesco Gigli <jaramir@gmail.com>
2019-06-06 14:13:23 +0200

    field renaming, step 5: remove the alias

--- content-admin/api/banner/controllers/Banner.js
+++ content-admin/api/banner/controllers/Banner.js
@@ -1,20 +1,3 @@
 'use strict'
 
-function add_aliases (response) {
-  return response
-    .map(object => object._doc)
-    .map(banner => Object.assign({color: banner.colour}, banner))
-}
-
-module.exports = {
-  find (ctx) {
-    if (ctx.query._q) {
-      return strapi.services.banner
-        .search(ctx.query)
-        .map(add_aliases)
-    }
-    return strapi.services.banner
-      .find(ctx.query)
-      .map(add_aliases)
-  }
-}
+module.exports = {}
